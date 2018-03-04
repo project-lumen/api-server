9b5dc9f65c048e2883311caa35ab1ee99bfe560f
Jeremie Soffichiti <33518344+Sophikitis@users.noreply.github.com>
2018-03-04 18:58:46 +0100

    Add colors text

--- scripts/mongoBackup.sh
+++ scripts/mongoBackup.sh
@@ -4,6 +4,16 @@
 # mongoBackup.sh
 # Permet d'exporter la base de donées ou d'importer une existante
 # -----------------------------------------------------------------
+
+# _______________________COLORS_
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+On_Black='\033[40m'  # Black
+BWhite='\033[1;37m'  # BOLD White
+# _______________________________
+
+
 echo -e "
 ===========================================
 *                                         *
@@ -25,13 +35,13 @@ _______________________________________________________________
 " choix1;
 if [[ $choix1 -eq 1 ]]
 then
-  echo "Ok super ! On passe a la suite mameneee !" ;
+  echo -e "${GREEN}${On_Black}Ok super ! On passe a la suite mameneee !${NC}" ;
 elif [[ $menu -eq 2 ]]
 then
-  echo "Ah ! Heureusement que je suis là ! " ;
+  echo -e "${GREEN}${On_Black}Ah ! Heureusement que je suis là ! ${NC}" ;
   sudo service mongod start
 else
- echo "T'es vraiment un boulet ! c'est pas compliqué : OUI = 1 / NON = 2";
+ echo -e "${RED}${On_Black}T'es vraiment un boulet ! c'est pas compliqué : OUI = 1 / NON = 2${NC}";
  bash mongoBackup.sh
 fi
 
@@ -53,14 +63,16 @@ then
   " choix2a;
   if [[ $choix2a -eq y ]]
   then
-    echo "Ok j'exporte la bdd dans le dossier data/backup";
+    echo -e "${GREEN}${On_Black}Ok j'exporte la bdd dans le dossier ${BWhite}data/backup${GREEN}${On_Black} et je fais ${BWhite}une copie avec la date / heure d'aujourd'hui ${NC}";
     sudo mongodump --out /var/www/html/data/backup/
+    sudo mongodump --out /var/www/html/data/$(date +%Y%m%d_%H:%M:%S)/backup/
+
   elif [[ $choix2a -eq n ]]
   then
-    echo "Ah ! Je viens peut etre d'eviter que tu fasses un connerie, non ?";
+    echo -e "${GREEN}${On_Black}Ah ! Je viens peut etre d'eviter que tu fasses un connerie, non ?${NC}";
     bash mongoBackup.sh
   else
-    echo "Bordel c'est pas compliqué y pour oui / n pour non ! "
+    echo -e "${RED}${On_Black}Bordel c'est pas compliqué y pour oui / n pour non !${NC} "
     bash mongoBackup.sh
   fi
 elif [[ $choix2 -eq 2 ]]
@@ -72,18 +84,18 @@ then
   " choix2b;
   if [[ $choix2b -eq y ]]
   then
-    echo "Ok j'importe la bdd depuis le dossier data/backup";
+    echo -e "${GREEN}${On_Black} Ok j'importe la bdd depuis le dossier ${BWhite}data/backup ${NC}";
     mongorestore --port 27017 /var/www/html/data/backup/
   elif [[ $choix2a -eq n ]]
   then
-    echo "Ah ! Je viens peut etre d'eviter que tu fasses un connerie, non ?";
+    echo -e "${GREEN}${On_Black}Ah ! Je viens peut etre d'eviter que tu fasses un connerie, non ?${NC}";
     bash mongoBackup.sh
   else
-    echo "Bordel c'est pas compliqué y pour oui / n pour non ! "
+    echo -e "${RED}${On_Black}Bordel c'est pas compliqué y pour oui / n pour non ! ${NC}"
     bash mongoBackup.sh
   fi
 elif [[ $choix2 -eq 3 ]]
 then
-  echo "Bye !";
+  echo -e "${GREEN}${On_Black}Bye !${NC}";
   exit;
 fi
