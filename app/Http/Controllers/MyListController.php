76098918976136e247bda4c431319595db637ec7
Adrien <33926737+afevre@users.noreply.github.com>
2018-03-09 09:05:28 +0100

    commentary

--- app/Http/Controllers/MyListController.php
+++ app/Http/Controllers/MyListController.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Controller;
 use App\myList;
 
 
-
+//initialisation de la class emp pour l'ajout des task
 class emp {
    public $idTask = "";
    public $titleTask  = "";
@@ -17,11 +17,13 @@ class emp {
 
 class MyListController extends Controller
 {
+      //récupération de la la liste en fonction du tokenList choisi
       public function printList(Request $request){
          $myList = myList::where('tokenList', "=", $request->input('tokenList'))->first();
          return $myList;
       }
 
+      //ajout d'une liste et d'une premiere task 
       public function addList(Request $request){
         if ($request->has('creator') && $request->has('nameList')) {
           $myList = new myList;
@@ -46,7 +48,8 @@ class MyListController extends Controller
           return "Saisir toutes les donneés";
         }
       }
-
+      
+      //Ajout d'une task grace à la class initialiser plus haut attention la task est un json
       public function addTask(Request $request){
         if ($request->has('tokenList')) {
           $myList = myList:: where("tokenList", "=", $request->input('tokenList'))
@@ -59,11 +62,14 @@ class MyListController extends Controller
             $task->flag  = 4;
             $task->dateStart = $request->input('dateStart');
             $task->dateEnd = $request->input('dateEnd');
-
+            
+             //ont encode l'objet task en json
             $newTask= json_encode($task);
 
+            //ont concaténe la nouvelle tache avec les anciennes
             $myList->task = $myList->task.','.$newTask;
             $myList->save();
+            // pour récupérer les task en json il faut la mettre entre [] 
             // var_dump(json_decode('['.$myList->task.']'));
             return "Nouvelle tache ajoutée";
           }
