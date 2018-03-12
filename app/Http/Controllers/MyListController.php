8dd2ababd3cd16281b22085a63ed533ad42ba35f
afevre <a.fevre@it-akademy.fr>
2018-03-12 15:51:01 +0100

    Add:checkTask

--- app/Http/Controllers/MyListController.php
+++ app/Http/Controllers/MyListController.php
@@ -25,16 +25,17 @@ class MyListController extends Controller
         $user = User::where("api_token", "=", $request->input('api_token'))->first();
 
         // var_dump($user->list);
-        foreach ($user->list as  $index => $value) {
-          $myList = myList::where('tokenList', "=", $value)->first();
-          $res[$index] = ['name' => $myList->nameList, 'task'=> $myList->task];
-
-
-
-
-          //   return $myList->creator;
-                      // return $myList;
-
+        if(!empty($user->list)){
+          $res['success']="valide";
+          foreach ($user->list as  $index => $value) {
+            $myList = myList::where('tokenList', "=", $value)->first();
+            $res[$index] = ['name' => $myList->nameList, 'tokenList'=> $myList->tokenList];
+            //   return $myList->creator;
+                        // return $myList;
+          }
+        }else{
+          $res['success']=false;
+          $res['message']="erreur";
         }
         return response($res);
 
@@ -51,39 +52,33 @@ class MyListController extends Controller
       //ajout d'une liste et d'une premiere task
       // param a envoyer : creator / nameList / api_token
       public function addList(Request $request){
-        if ($request->has('creator') && $request->has('nameList')) {
-          $myList = new myList;
-          $myList->creator=$request->input('creator');
-          $myList->nameList=$request->input('nameList');
-          $myList->tokenList=str_random(16);
-            $task = new emp();
-            $task->idTask = str_random(10);
-            $task->titleTask  = "Ma premiere Tache";
-            $task->check  = true;
-            $task->flag  = 4;
-            $task->dateStart = "12/12/2012";
-            $task->dateEnd = "13/13/2013";
-            $newTask= json_encode($task);
-          $myList->task =$newTask;
-          if($myList->save()){
-            $user = User::where("api_token", "=", $request->input('api_token'))->first();
-
-              $arrayName =$user->list ;
-              array_push($arrayName, $myList->tokenList);
-              var_dump($arrayName);
-
-
-              $user->list= $arrayName;
-
-            $user->save();
-            return "Liste créée";
-          } else {
-            return "Probleme d'insertion de la Liste";
-          };
-        }else{
-          return "Saisir toutes les donneés";
-        }
-      }
+             if ($request->has('api_token') && $request->has('nameList') && $request->input('nameList') != null) {
+               $user = User::where("api_token", "=", $request->input('api_token'))->first();
+               $myList = new myList;
+               $myList->creator=$user->pseudo;
+               $myList->nameList=$request->input('nameList');
+               $myList->tokenList=str_random(16);
+               $myList->task =[];
+               if($myList->save()){
+                 $user = User::where("api_token", "=", $request->input('api_token'))->first();
+                 $arrayName = $user->list ;
+                 array_push($arrayName, $myList->tokenList);
+                 $user->list= $arrayName;
+                 $user->save();
+                 $res['success'] = true;
+                 $res['message'] = "Liste ajouté";
+                 return response($res);
+               } else {
+                 $res['success'] = false;
+                 $res['message'] = "Probleme insertion de liste";
+                 return response($res);
+               };
+             }else{
+               $res['success'] = false;
+               $res['message'] = "Saisir toutes les données";
+               return response($res);
+             }
+           }
 
       //Ajout d'une task grace à la class initialiser plus haut attention la task est un json
       public function addTask(Request $request){
@@ -99,18 +94,79 @@ class MyListController extends Controller
             $task->dateStart = $request->input('dateStart');
             $task->dateEnd = $request->input('dateEnd');
 
-             //ont encode l'objet task en json
-            $newTask= json_encode($task);
 
-            //ont concaténe la nouvelle tache avec les anciennes
-            $myList->task = $myList->task.','.$newTask;
+            $previusTask = $myList->task;
+            array_push($previusTask, $task);
+
+            $myList->task = $previusTask;
             $myList->save();
-            // pour récupérer les task en json il faut la mettre entre []
-            // var_dump(json_decode('['.$myList->task.']'));
-            return "Nouvelle tache ajoutée";
+
+            $res['success']=true;
+            $res['message']="Tache ajoutée";
+
+          }else {
+            $res['success']=false;
+            $res['message']="erreur";
+
           }
         }
+      return response($res);
       }
+
+
+
+
+      public function checkTask(Request $request){
+        if ($request->has('tokenList')) {
+          $myList = myList:: where("tokenList", "=", $request->input('tokenList'))
+                        ->first();
+
+              $truite = $myList->task;
+
+
+              foreach($truite as $item)
+              {
+                    if($item["idTask"] == $request->input('idTask'))
+                    {
+                      $item["check"]=$request->input('check');
+                        $previusTask = $myList->task;
+                        array_push($previusTask, $item);
+                        $myList->task = $previusTask;
+                      $mylist->save();
+                    }
+              }
+
+
+              $res['success']=true;
+              $res['message']="Changement de status effectuer";
+      }else {
+        $res['success']=false;
+        $res['message']="erreur";
+      }
+    return response($res);
+    }
+
+
+
+
+
+
+
+
+
+
+    public function testTask(Request $request){
+      $user = User::where("api_token", "=", $request->input('api_token'))->first();
+      foreach ($user["list"] as $key ) {
+
+          $myList = myList:: where("tokenList", "=", $key)
+                            ->first();
+          var_dump($myList["task"]);
+
+      }
+      return ("bite");
+  }
+
 
 
 }
